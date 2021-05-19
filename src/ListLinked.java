ee181581123821e989dc2c61fa141e342984c5a6
unknown <andre.campos@ucsm.edu.pe>
2021-05-18 22:28:15 -0500

    Avance del trabajo: no terminado

--- src/ListLinked.java
+++ src/ListLinked.java
@@ -5,6 +5,7 @@ public class ListLinked <T> implements TDAList<T>{
 	
 	public ListLinked() {
 		this.first= null;
+		this.count=0;
 	}
 	public boolean isEmptyList() {
 		return this.first == null;
@@ -17,16 +18,43 @@ public class ListLinked <T> implements TDAList<T>{
 			this.first=this.first.getNext();
 	}
 	public int search(T x) {
-		
+		Node<T> aux = this.first;
+		for(int i=0; aux != null; aux= aux.getNext(),i++)
+			if(aux.getData().equals(x))
+				return i;
+		return -1;
 	}
 	public void insertLast(T x) {
-		
+		if(this.isEmptyList())
+			this.insertFirst(x);
+		else {
+			Node<T> lastNode = getLastNode();
+			lastNode.setNext(new Node<T>(x));
+			this.count++;
+			
+		}	
+	}
+	private Node<T> getLastNode(){
+		Node<T> aux= this.first;
+		while(aux.getNext()!=null)
+			aux=aux.getNext();
+		return aux;
 	}
 	public void insertFirst(T x) {
+		this.first = new Node<T>(x,this.first);
+		this.count++;
 		
 	}
 	public void remove(T x) {
 		
+	}
+	
+	public String toString() {
+		String str="";
+		Node<T> aux = this.first;
+		for(int i=0;aux!=null;aux=aux.getNext(), i++)
+			str += "["+i+"] = "+aux.getData()+"\n";
+		return str;
 	}
 	
 }
--- src/TestList.java
+++ src/TestList.java
@@ -3,10 +3,18 @@ public class TestList {
 	
 	public static void main(String[] args) {
 		
-		TDAList <Integer> l1= new ListArray<Integer>(5);
+		TDAList <Integer> l1= new ListLinked<Integer>();
 		
-		l1.insertLast(10);
+	
+		l1.insertLast(3);
+		l1.insertLast(2);
+		l1.insertLast(4);
+
+
+		
 		System.out.println(l1);
+		System.out.println(l1.search(4));
+		
 		
 	}
 
